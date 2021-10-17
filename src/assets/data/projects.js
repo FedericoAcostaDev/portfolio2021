301a2f0d2ca656423efe661e81844a9665197880
Federico <fedeacos045@gmail.com>
2021-10-17 17:13:28 -0300

    added gradient to photos

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
+import Trade from '../images/trade.png';
 import Yolo from '../images/Yolo.png';
 import GreenCtgImg from '../images/greenctg.jpg';
 import Face from '../images/Face.png';
@@ -14,9 +14,8 @@ const projects = [
   {
     id: uuidv4(),
     name: 'My Crypto Tracker',
-    desc:
-      'An app to help people to get an overview of how they can make the city beautiful.',
-    img: GreenCtgImg,
+    desc: 'An App to see the Crypto market. React.js/Chart.js',
+    img: Trade,
   },
   {
     id: uuidv4(),
@@ -30,7 +29,7 @@ const projects = [
     name: 'Tracking Soft',
     desc:
       'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    img: ProjectImg,
+    img: GreenCtgImg,
   },
 ];
 
