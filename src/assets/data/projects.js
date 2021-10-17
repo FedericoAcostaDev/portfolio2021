2ac4620d13a965a86be70461ca32eb478cb0ec4a
Federico <fedeacos045@gmail.com>
2021-10-17 16:08:37 -0300

    projects updated

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,38 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
 import ProjectImg from '../images/projectImg.png';
-import UTrackerImg from '../images/utracker.jpg';
+import Yolo from '../images/Yolo.png';
 import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
+import Face from '../images/Face.png';
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'U Tracker',
-    desc:
-      'An application to track your all data from one place. I developed the website and the mobile app',
-    img: UTrackerImg,
+    name: 'YoLo ecommerce',
+    desc: ' chlotes for women, created in React.js',
+    img: Yolo,
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
+    name: 'My Crypto Tracker',
     desc:
       'An app to help people to get an overview of how they can make the city beautiful.',
     img: GreenCtgImg,
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
-    desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    img: CoinTrackerImg,
-  },
-  {
-    id: uuidv4(),
-    name: "Cavin's Portfolio",
+    name: 'Face Detector',
     desc:
       'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    img: CavinImg,
+    img: Face,
   },
   {
     id: uuidv4(),
